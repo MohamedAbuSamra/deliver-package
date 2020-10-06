8dd79ea1c85a9c098427c61e15807fd0a95c7e29
Mohamed Abu Samra <43873183+MohamedAbuSamra@users.noreply.github.com>
2020-10-06 22:02:20 +0300

    movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private CharacterController character_controller;
+    private Vector3 move_Direction;
+    public float speed = 5f;
+    private float gravity = 20f;
+    public float vertical_velocity;
+
+    void Awake(){
+        character_controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    void MovePlayer(){
+        move_Direction = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+
+        move_Direction = transform.TransformDirection(move_Direction);
+        move_Direction *= speed * Time.deltaTime;
+        character_controller.Move(move_Direction);
+    }
+}
---
+++ Assets/movementplayer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class movementplayer : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    void FixedUpdate(){
+
+rb.AddForce(0,0,  1000 * Time.deltaTime);
+if (Input.GetKey("a"))
+        {
+            rb.AddForce(-500 * Time.deltaTime, 0, 0);
+        }
+if (Input.GetKey("d"))
+        {
+            rb.AddForce(500 * Time.deltaTime, 0, 0);
+        }
+if (Input.GetKey("s"))
+        {
+            rb.AddForce(0,0,  -1000 * Time.deltaTime);
+        }
+    
+}
+}
